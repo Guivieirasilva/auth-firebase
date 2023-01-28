f95fe886248001ffeb330d85c259318e08037a7b
guivieirasilva <guivieirasilva02@gmail.com>
2023-01-28 16:54:31 -0300

    auth-firebase

---
+++ src/auth/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9M65tVjQB2UDzFl-uEC9YMBfOXg24UsY",
+  authDomain: "auth-bfc39.firebaseapp.com",
+  projectId: "auth-bfc39",
+  storageBucket: "auth-bfc39.appspot.com",
+  messagingSenderId: "1006182189151",
+  appId: "1:1006182189151:web:a043bbb571adfc4d0d73ea",
+  measurementId: "G-S8XTETKSQ2"
+};
+
+export const app = initializeApp(firebaseConfig);
